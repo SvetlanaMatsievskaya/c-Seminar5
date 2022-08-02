37f0c3b6b722ef3aeab3b80063474c9e099f24aa
svetlana matsyevskaya <svetlanamats@mail.ru>
2022-08-02 21:36:52 +0300

    Task 34 completed

--- Task34/Program.cs
+++ Task34/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int CountPositiveNumbers(int[] col)
+{
+  int CountPositive = 0;
+  for (int j = 0; j < col.Length; j++)
+{
+    if (col[j]%2 == 0) CountPositive+=1;
+  
+}
+return CountPositive;  
+
+}
+ 
+void FillArray (int[] array)
+{
+  for (int i = 0; i < array.Length; i++)
+{
+  array[i] = new Random().Next(100,1000);
+  Console.Write($"{array[i]} ");
+}
+Console.WriteLine();
+}
+int[] arr = new int[5];
+FillArray(arr);
+Console.WriteLine(CountPositiveNumbers(arr));

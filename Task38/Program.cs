288b06a5585f2f0dbe483092a9ca942144321cdf
svetlana matsyevskaya <svetlanamats@mail.ru>
2022-08-02 22:32:35 +0300

    Task 38 added

---
+++ Task38/Program.cs
@@ -0,0 +1,14 @@
+﻿
+int Max(int[]matr)
+{
+    int m = 0;
+    for (int i = 0; i < matr.Length; i++)
+    {
+        if (matr[i] > m) m = matr[i];
+         
+    }
+return(m);
+}
+
+int[] matrix = {5,10,356,57,9,1};
+Console.WriteLine(Max(matrix));
